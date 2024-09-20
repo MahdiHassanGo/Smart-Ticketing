4137ae84b5c44f7afcd57a2e14042085486b6bdf
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-09-20 18:15:25 +0600

    third  commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const menuBtn= document.getElementById('menu-btn');
 const mobileMenuEl = document.getElementById('mobileMenu');
-
+const totalBookEl=document.getElementById('total-booked')
 const selectedSeatEl = document.getElementById ('selected-seat');
-
+const availableSeatEl= document.getElementById('available-seat');
 
 menuBtn .addEventListener('click',function(){
     menuBtn.children[0].classList.toggle('hidden');
@@ -14,14 +14,27 @@ menuBtn .addEventListener('click',function(){
     mobileMenuEl.classList.toggle('hidden');
     mobileMenuEl.classList.toggle('flex');
 })
-
+let selectedSeat= [];
 
 function handleSelectSeat (event){
-    console.log(event.innerText);
-    selectedSeatEl.innerHTML=`<li class="text-base font-normal flex justify-between">
-<span>${event.innerText}</span>
-<span>Economy</span>
-<span>550</span>
+ 
 
-    </li>`
+event.classList.add('bg-primary');
+event.classList.add('text-white');
+
+selectedSeat.push(event.innerText);
+totalBookEl.innerText=selectedSeat.length;
+const availableSeatValue = parseFloat(availableSeatEl.innerText);
+const newAvailableSeatValue = availableSeatValue-1;
+availableSeatEl.innerText= newAvailableSeatValue  ;  
+
+
+
+
+    //     selectedSeatEl.innerHTML=`<li class="text-base font-normal flex justify-between">
+// <span>${event.innerText}</span>
+// <span>Economy</span>
+// <span>550</span>
+
+//     </li>`
 }
